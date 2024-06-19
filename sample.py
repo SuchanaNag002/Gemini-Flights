7ba2ce350940171440c78ab32c6db6982b15dbf4
Suchana Nag <99524477+SuchanaNag002@users.noreply.github.com>
2024-06-19 11:40:13 +0530

    Update  model name in sample.py

--- sample.py
+++ sample.py
@@ -48,7 +48,7 @@ config = generative_models.GenerationConfig(temperature=0.4)
 
 # Load model with config
 model = GenerativeModel(
-    "gemini-pro",
+    "gemini-1.5-flash-001",
     tools=[search_tool],
     generation_config=config
 )
